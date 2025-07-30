af1cf1760bfd192f5a50a3438012dd2da4140b71
Yunom Yxvia <yunomix2834@gmail.com>
2025-07-31 02:59:43 +0700

    Update Compose

--- ai-service/src/main/java/com/codecampus/ai/config/DualPortConfig.java
+++ ai-service/src/main/java/com/codecampus/ai/config/DualPortConfig.java
@@ -1,30 +1,30 @@
-package com.codecampus.ai.config;
-
-import org.apache.catalina.connector.Connector;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DualPortConfig {
-    @Value("${server.http.port}")
-    private int httpPort;
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        // Tomcat là default container của Spring Boot Starter Web
-        TomcatServletWebServerFactory factory =
-                new TomcatServletWebServerFactory();
-
-        Connector httpConnector =
-                new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        httpConnector.setScheme("http");
-        httpConnector.setPort(httpPort);
-        httpConnector.setSecure(false);
-
-        factory.addAdditionalTomcatConnectors(httpConnector);
-        return factory;
-    }
-}
+//package com.codecampus.ai.config;
+//
+//import org.apache.catalina.connector.Connector;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+//import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DualPortConfig {
+//    @Value("${server.http.port}")
+//    private int httpPort;
+//
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        // Tomcat là default container của Spring Boot Starter Web
+//        TomcatServletWebServerFactory factory =
+//                new TomcatServletWebServerFactory();
+//
+//        Connector httpConnector =
+//                new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//        httpConnector.setScheme("http");
+//        httpConnector.setPort(httpPort);
+//        httpConnector.setSecure(false);
+//
+//        factory.addAdditionalTomcatConnectors(httpConnector);
+//        return factory;
+//    }
+//}
