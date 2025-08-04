777d67ccafa6881b103220d12dc53fac5ffdc223
nhatl <nhatluu52351412@gmail.com>
2025-08-05 03:56:09 +0700

    create base project for post service

---
+++ post-service/src/main/java/com/codecampus/post/constant/exception/ErrorCodeConstant.java
@@ -0,0 +1,18 @@
+package com.codecampus.post.constant.exception;
+
+import lombok.NoArgsConstructor;
+
+import static io.grpc.Status.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
+
+@NoArgsConstructor
+public class ErrorCodeConstant {
+    public static final String INTERNAL_SERVER_STATUS =
+            INTERNAL_SERVER_ERROR.toString();
+    public static final String BAD_REQUEST_STATUS = BAD_REQUEST.toString();
+    public static final String UNAUTHORIZED_STATUS =
+            org.springframework.http.HttpStatus.UNAUTHORIZED.toString();
+    public static final String FORBIDDEN_STATUS = FORBIDDEN.toString();
+    public static final String NOT_FOUND_STATUS = NOT_FOUND.toString();
+    public static final String CONFLICT_STATUS = CONFLICT.toString();
+}
